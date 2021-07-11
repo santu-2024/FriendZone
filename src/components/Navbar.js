d9b5be5116481973d2cec6b6d0a108b3cb7634ea
Santu <santumete01@gmail.com>
2021-07-11 21:49:58 +0530

    add

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,7 @@ const Navbar = () => {
             <div className = "logo-nav"><img src= "logo.png" alt= "logo"
             ></img></div>
             app
+            santu
         </div>
     )
 }
