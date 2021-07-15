3c0f822e7a850a1e7bfb2de5b933c6538b2bcab6
Santu <santumete01@gmail.com>
2021-07-15 22:45:02 +0530

    responsive image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,9 @@ import React from 'react'
 const Navbar = () => {
     return (
         <div>
-            <div className = "logo-nav"><img src= "logo.png" alt= "logo"
-            ></img></div>
-            app
-            santu
-            klk
-            loll
+            {/* <div className = "logo-nav"><img src= "logo.png" alt= "logo"
+            ></img></div> */}
+            <img src="me.png" alt="logo"/>
         </div>
     )
 }
