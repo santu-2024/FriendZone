f7b71a631a312a6f962a0dd25085d7259f6960af
Santu <santumete01@gmail.com>
2021-07-20 17:27:37 +0530

    responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,9 +7,6 @@ const Navbar = () => {
             {/* <div className = "logo-nav"><img src= "logo.png" alt= "logo"
             ></img></div> */}
             <img src="me.png" alt="logo"/>
-            app
-            do
-            kgdkw
         </div>
     )
 }
